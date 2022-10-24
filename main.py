345663e148a9e4c4c13f1a38830e9d728cd4a076
EpicPlayzGames <danielsgame.media@gmail.com>
2022-10-24 00:10:26 -0700

    Added variable list to cycle through within if/elif statements and check for operator value

--- main.py
+++ main.py
@@ -2,42 +2,43 @@ from math import *
 
 operator = input("Enter the operation: ")
 variable_type = float
+variables = ["sqrt", "+", "-", "*", "/"]
 
 num1 = ""
 num2 = ""
 
-if operator == "sqrt":
+if operator == variables[0]:
 
     num1 = input("Enter a value: ")
     print(sqrt(variable_type(num1)))
 
-elif operator == "-":
-
-    num1 = input("Enter your first number: ")
-    num2 = input("Enter your second number: ")
-
-    print(variable_type(num1) - variable_type(num2))
-
-elif operator == "*":
-
-    num1 = input("Enter your first number: ")
-    num2 = input("Enter your second number: ")
-
-    print(variable_type(num1) * variable_type(num2))
-
-elif operator == "/":
-
-    num1 = input("Enter your first number: ")
-    num2 = input("Enter your second number: ")
-
-    print(variable_type(num1) / variable_type(num2))
-
-elif operator == "+":
+elif operator == variables[1]:
 
     num1 = input("Enter your first number: ")
     num2 = input("Enter your second number: ")
 
     print(variable_type(num1) + variable_type(num2))
 
-elif operator != "sqrt" or "+" or "-" or "*" or "/":
+elif operator == variables[2]:
+
+    num1 = input("Enter your first number: ")
+    num2 = input("Enter your second number: ")
+
+    print(variable_type(num1) - variable_type(num2))
+
+elif operator == variables[3]:
+
+    num1 = input("Enter your first number: ")
+    num2 = input("Enter your second number: ")
+
+    print(variable_type(num1) * variable_type(num2))
+
+elif operator == variables[4]:
+
+    num1 = input("Enter your first number: ")
+    num2 = input("Enter your second number: ")
+
+    print(variable_type(num1) / variable_type(num2))
+
+elif operator != variables:
     print("Unknown operator, please try again.")
